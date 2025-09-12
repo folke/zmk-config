26f57fd19f6b77c2c8f02da5a907ffc63ee89d2c
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-09-12 20:21:30 +0200

    refactor: optimize for chocofi

--- config/includes/keys.h
+++ config/includes/keys.h
@@ -52,3 +52,7 @@
 #define RH0 33  // right thumb keys
 #define RH1 34
 #define RH2 35
+
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left hand
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right hand
+#define KEYS_T LH2 LH1 LH0 RH0 RH1 RH2                                      // thumbs
